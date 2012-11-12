0a47bf32e9c3eaa8dca5ae299f977a605cc468b0
windsdeng <windsdeng@hotmail.com>
2012-11-12 09:43:56 +0800

    Revert "update"
    This reverts commit d6319a824dcdf7797687027c66991a98928e0995.

--- views/medium_login.php
+++
@@ -1,6 +0,0 @@
-<div class="auth-login-yii">
-	<p>
-        <a class="auth-medium-yii" href="<?php echo $this->sina_code_url ?>"　title="新浪微博"><img src="<?php echo $this->baseUrl;?>/images/sina_logo.png" alt="新浪微博">新浪微博</a>
-        <a class="auth-medium-yii" href="<?php echo $this->qq_code_url ?>"　title="腾讯互联"><img src="<?php echo $this->baseUrl;?>/images/qq_logo.png" alt="腾讯互联">腾讯互联</a>
-    </p>
-</div>
