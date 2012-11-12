f56855797858e630e868ab13a234cc2229860505
windsdeng <windsdeng@hotmail.com>
2012-11-12 09:56:28 +0800

    update

---
+++ config/main.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @var 新浪配置
+ */
+define( "WB_AKEY" , '********' );
+define( "WB_SKEY" , '****************************************' );
+define( "WB_CALLBACK_URL" , 'http://demo.dlf5.net/site/wblogin');
+
+/**
+ * @var QQ配置
+ */
+
+define( "QQ_APPID" , '********' );
+define( "QQ_APPKEY" , '****************************************' );
+define( "QQ_CALLBACK_URL" ,'http://demo.dlf5.net/site/qqlogin');
+
